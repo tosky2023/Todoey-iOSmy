c529f57feba918891fea62d9356355df983bd4d5
tosky2023 <126580146+tosky2023@users.noreply.github.com>
2023-04-18 10:47:26 +0800

    add  code able

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -5,23 +5,21 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-    let defaults = UserDefaults.standard
     
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        let newItem = Item()
+        print(dataFilePath)
         
-//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-//            itemArray = items
-//        }
+       
+        loadItems()
     }
-
-//MARK: - TableView Datasource Methods
+    
+    //MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -30,31 +28,31 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
         
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        self.saveItems()
         return cell
     }
     
-//MARK: - TableView Delegate Methods
+    //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-
-        }
+        saveItems()
+        
         
         tableView.deselectRow(at: indexPath, animated: true)
         
-
+        
     }
-
-//MARK: - Add New Items
+    
+    //MARK: - Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -65,11 +63,12 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "add Item", style: .default) { (action) in
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.itemArray.append(newItem)
+            self.saveItems()
             
-            self.tableView.reloadData()
         }
         
         alert.addTextField{ (alertTextField) in
@@ -82,6 +81,34 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         
         present(alert,animated: true,completion: nil)
+    }
+    
+    
+    //MARK: - Model Manupulation Method
+    
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to:dataFilePath!)
+        } catch {
+            print("Error encoding item array \(error)")
+        }
+        
+        self.tableView.reloadData()
+        
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray =  try decoder.decode([Item].self, from: data)
+            } catch {
+                print("error  decoing item array,\(error)")
+            }
+        }
     }
     
     
--- Todoey/DataModel/Item.swift
+++ Todoey/DataModel/Item.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-class Item {
-    var done:Bool = true
+class Item : Codable {
     var title:String = ""
+    var done:Bool = false
 
 }
