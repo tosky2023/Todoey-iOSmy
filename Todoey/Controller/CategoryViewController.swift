b34add373ca96d12ece24b9b16c66f41baeeb1c9
tosky2023 <126580146+tosky2023@users.noreply.github.com>
2023-04-22 22:22:20 +0800

    complet  data manipulation  add new

--- Todoey/Controller/CategoryViewController.swift
+++ Todoey/Controller/CategoryViewController.swift
@@ -21,28 +21,31 @@ class CategoryViewController: UITableViewController {
         
     }
     
-//MARK: - TableView Datasource Method
+    //MARK: - TableView Datasource Method
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
-       }
-    
+    }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>,for:indexPath)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell",for:indexPath)
         
         let category = categories[indexPath.row]
         
-  
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+        
     }
-
-   
-
-//MARK: - Data Manipulation Methods
-
+    
+    
+    
+    //MARK: - Data Manipulation Methods
+    
     func saveCategories() {
         do {
-            
             try context.save()
         } catch {
             print("error in saveing \(error)")
@@ -50,11 +53,11 @@ class CategoryViewController: UITableViewController {
         
         self.tableView.reloadData()
     }
-
+    
     func loadCategories(with request:NSFetchRequest<Category> = Category.fetchRequest()){
-         
+        
         do {
-           categories = try context.fetch(request)
+            categories = try context.fetch(request)
         } catch  {
             print(" error featch data from context\(error)")
         }
@@ -65,12 +68,37 @@ class CategoryViewController: UITableViewController {
     
     
     
-
-//MARK: - Add New Categories
-
+    
+    //MARK: - Add New Categories
+    
     
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle:.alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            
+            self.categories.append(newCategory)
+            
+            
+            self.saveCategories()
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a new category"
+        }
+        
+        present(alert, animated: true, completion: nil)
+        
     }
     
     
@@ -84,4 +112,4 @@ class CategoryViewController: UITableViewController {
     
     
 }
-   
+
